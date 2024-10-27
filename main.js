e09a4de5fa86f98647713f8e36c5b32a71f9519a
coryrmrz <coryramirez2018@gmail.com>
2024-10-27 17:47:39 -0400

    Add event listener for product selection and quantity input

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 //Task 1: Setup HTML structure for order form
 // ^look at index.html for task 1
+
+//Task 2: Add Event Listener for Product Selection
+const productSelector = document.getElementById('product-selector'); //retrieves HTML element: product-selector
+const quantityInput = document.getElementById('quantity'); //retrieves HTML element: quantity
+const totalPriceElement = document.getElementById('total-price'); //retrieves HTML element: total-price
+const placeOrderButton = document.getElementById('place-order'); //retrieves HTML element: place-order
+const orderSummary = document.getElementById('order-summary'); //retrieves HTML element: order-summary
